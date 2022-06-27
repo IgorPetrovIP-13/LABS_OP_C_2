4b32a73d14818c70b80167bc6b28352f4eb85dbb
IgorPetrovIP-13 <90032166+IgorPetrovIP-13@users.noreply.github.com>
2022-06-28 00:22:06 +0300

    Add files via upload

---
+++ LAB_6/func.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(string);
+void fixed_expression(vector<string>);
+bool is_operator(string);
